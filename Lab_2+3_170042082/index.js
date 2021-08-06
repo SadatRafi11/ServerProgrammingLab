aeeb364b4884795bc0966173d2598deea734097e
Sadat Rafi <rafisadat41@gmail.com>
2021-08-06 14:10:31 +0600

    Completion of lab_tasks of lab 04

--- Lab_2+3_170042082/index.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./app.js");
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-    console.log(`up and running at port ${PORT}`);
-});
--- Lab_2+3_170042082/routes/indexRoutes.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getHome, defRoute } = require("./../controllers/indexController.controller");
-
-router.get ("/", getHome);
-
-router.use(defRoute);
-
-module.exports = router;
